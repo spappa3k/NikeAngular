f7c4d05cea867e844518c07ef91587c1f98c31a1
spappa3k <pappalardo.dev@gmail.com>
2024-08-21 11:02:13 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NikeService } from './nike.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'NikeAngular';
+
+  constructor(private ns:NikeService){}
+
+  ngOnInit(): void {
+   // this.ns.allProducts().subscribe();
+  }
 }
---
+++ src/assets/models/models.ts
@@ -0,0 +1,13 @@
+  export interface Prodotti {
+    id: number
+    nome: string
+    categoria: string
+    prezzo: number
+    taglie_disponibili: string[]
+    colori_disponibili: string[]
+    descrizione: string
+    immagine: string
+    nuovo_arrivi: boolean
+    best_seller: number
+  }
+  
