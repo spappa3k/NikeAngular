482f3c073de8cd0c2c850d927ba803d2b7965827
spappa3k <pappalardo.dev@gmail.com>
2024-08-25 15:52:59 +0200

    .

--- src/app/shoes-details/shoes-details.component.ts
+++ src/app/shoes-details/shoes-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NikeService } from '../nike.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Prodotti } from '../../assets/models/models';
+import { style } from '@angular/animations';
 
 @Component({
   selector: 'app-shoes-details',
@@ -11,14 +12,22 @@ import { Prodotti } from '../../assets/models/models';
 export class ShoesDetailsComponent{
 prodotto?:Prodotti;
 
-
   constructor(private ns:NikeService, private route:ActivatedRoute){
+    /* GET ID*/
     const id= this.route.snapshot.paramMap.get('id');
     const idNumber=id ? +id : null;  /* convert to number */
-  this.prodotto=this.ns.searchById(idNumber!);
-console.log("prodotto arrivato", this.prodotto);
 
+/* CALL TO SERVICE*/
+  this.ns.searchById(idNumber!)
+  .subscribe(data=>{
+    this.prodotto=data;
+    console.log("Photo:", this.prodotto.immagine);
+  })
+  
+console.log("prodotto arrivato", this.prodotto);
 ns.viewBannerHearderOnOff(false);
   }
+
+
 }
 
