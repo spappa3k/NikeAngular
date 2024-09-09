c6f4ae6301723d982a771c514639cf5ee3dcfe0e
spappa3k <pappalardo.dev@gmail.com>
2024-09-09 19:57:21 +0200

    .

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgSelectOption } from '@angular/forms';
+import { NikeService } from '../nike.service';
+import { ProdottoForCheckout } from '../../assets/models/models';
 
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
   styleUrl: './basket.component.css'
 })
-export class BasketComponent {
+export class BasketComponent implements OnInit{
+prodottiInBasket:ProdottoForCheckout[]=[]
+
+  constructor(private ns:NikeService){
+  ns.viewBannerHearderOnOff(false);
+  }
+
+  ngOnInit(): void {
+    this.prodottiInBasket=this.ns.prodottiInBasket;
+  }
 
 }
