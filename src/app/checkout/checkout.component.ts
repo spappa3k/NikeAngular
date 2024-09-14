40d9b865ddcaeaaf4314e333ffccb5f7eaadf13a
spappa3k <pappalardo.dev@gmail.com>
2024-09-14 15:31:27 +0200

    .

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -2,17 +2,50 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { NikeService } from '../nike.service';
 
+
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent implements OnInit{
-formCheckout:FormGroup;
+formCheckoutPersonalInfo:FormGroup;
+formCheckoutShipping:FormGroup;
+
+cities = [
+  "Birmingham",
+  "Bradford",
+  "Brighton",
+  "Bristol",
+  "Cambridge",
+  "Coventry",
+  "Derby",
+  "Glasgow",
+  "Hull",
+  "Leeds",
+  "Leicester",
+  "Liverpool",
+  "London",
+  "Manchester",
+  "Newcastle",
+  "Norwich",
+  "Nottingham",
+  "Oxford",
+  "Plymouth",
+  "Portsmouth",
+  "Reading",
+  "Sheffield",
+  "Southampton",
+  "Stoke-on-Trent",
+  "Sunderland",
+  "Wakefield",
+  "Wolverhampton"
+];
+
 
 constructor(private ns:NikeService){
-  
-  this.formCheckout= new FormGroup({
+
+  this.formCheckoutPersonalInfo= new FormGroup({
 FirstName:new FormControl(''),
 LastName:new FormControl(''),
 DateOfBirth:new FormControl(''),
@@ -20,12 +53,20 @@ Gender:new FormControl(''),
 Email:new FormControl(''),
 PhoneNumber:new FormControl('')
   });
+
+  this.formCheckoutShipping= new FormGroup({
+    StreetAddress:new FormControl(''),
+    AdditionalAddress:new FormControl(''),
+    CivicNumber:new FormControl(''),
+    City:new FormControl(''),
+    CAP:new FormControl(''),
+      });
 }
 ngOnInit(): void {
   this.ns.viewBannerHearderOnOff(false);
 }
 
 OnSubmit(){
-  console.log(this.formCheckout.value);
+  console.log(this.formCheckoutPersonalInfo.value);
 }
 }
