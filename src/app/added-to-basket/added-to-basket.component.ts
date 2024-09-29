97c2583d3bb11dc0cae8b28b38bc3e0a8e3b5228
spappa3k <pappalardo.dev@gmail.com>
2024-09-29 13:52:30 +0200

    .

--- src/app/added-to-basket/added-to-basket.component.ts
+++ src/app/added-to-basket/added-to-basket.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, HostListener } from '@angular/core';
 import { Prodotti, ProdottoForCheckout } from '../../assets/models/models';
 import { NikeService } from '../nike.service';
 
@@ -9,7 +9,11 @@ import { NikeService } from '../nike.service';
 })
 
 export class AddedToBasketComponent implements OnInit{
+  public getScreenWidth: any;
+  public getScreenHeight: any;
+  dontShow=false;
 
+  
 @Input()
 prodottoToPush?:ProdottoForCheckout
 
@@ -23,6 +27,10 @@ constructor(private ns:NikeService){
 
 ngOnInit(): void {
   this.pushIt();
+
+  
+  this.getScreenWidth = window.innerWidth;
+  this.getScreenHeight = window.innerHeight;
 }
 
 pushIt(){
@@ -30,4 +38,14 @@ pushIt(){
   console.log("Prodotto pushato: ", this.prodottoToPush);
 }
 
+
+@HostListener('window:resize', ['$event'])
+onWindowResize() {
+  this.getScreenWidth = window.innerWidth;
+  this.getScreenHeight = window.innerHeight;
+  console.log("width: ",this.getScreenWidth);
+  if(this.getScreenWidth<768){
+    this.dontShow=true;
+  }
+}
 }
